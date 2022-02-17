01059675ec6da7d479ef992f1b11e5f3f6d8d00b
MD-MAHBUBUR-RAHMAN <nirjhor211288@gmapl.com>
2022-02-17 20:41:25 +0600

    Modified All

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//Common Input Function:
+///////////////////////////=======Common Input Function Creation==========\\\\\\\\\\\\\\\\\\\\
 function commonInput(inputId) {
     const inputItem = document.getElementById(inputId);
     if (inputItem.value < 0 || inputItem.value == "" || isNaN(inputItem.value)) {
@@ -7,12 +7,12 @@ function commonInput(inputId) {
     else {
         const inputValue = inputItem.value;
         inputItem.value = inputValue;
-        const finalValue = parseInt(inputItem.value);
+        const finalValue = parseFloat(inputItem.value);
         //return function
         return finalValue;
     }
-
 }
+///////////////////////////=======Expense Calculation==========\\\\\\\\\\\\\\\\\\\\
 function calculationExpense() {
     //Food Input:=======================
     const foodExpense = commonInput("food");
@@ -21,9 +21,14 @@ function calculationExpense() {
     //Clothes Inpute:======================
     const clothExpense = commonInput("cloth");
 
-    const TotalExpence = foodExpense + rentExpense + clothExpense;
-    return TotalExpence;
+    if (typeof foodExpense === 'undefined' || typeof rentExpense === 'undefined' || typeof clothExpense === 'undefined') {
+        document.getElementById("total-expense").innerText = "0"
+    } else {
+        const TotalExpence = foodExpense + rentExpense + clothExpense;
+        return TotalExpence;
+    }
 }
+///////////////////////////=======Balance Calculation==========\\\\\\\\\\\\\\\\\\\\
 function calculateBalance() {
     //Income input========================
     const income = commonInput("income");
@@ -31,49 +36,43 @@ function calculateBalance() {
     return balance;
 
 }
+///////////////////////////=======Main Calculation==========\\\\\\\\\\\\\\\\\\\\
 
-//Main Calculation Part:
 document.getElementById("calculate").addEventListener("click", function () {
-    // //Income input========================
-    // const income = commonInput("income");
-    // //Food Input:=======================
-    // const foodExpense = commonInput("food");
-    // //Rent Input:==========================
-    // const rentExpense = commonInput("rent");
-    // //Clothes Inpute:======================
-    // const clothExpense = commonInput("cloth");
+
     //Total Expenses:========================
     const totalInnerText = document.getElementById("total-expense");
     totalInnerText.innerText = calculationExpense()
-    // totalInnerText.innerText = foodExpense + rentExpense + clothExpense;
-    const totalExpenses = parseInt(totalInnerText.innerText);
-    //Balance Amount:========================================
-    const balanceInnerText = document.getElementById("balance");
-    balanceInnerText.innerText = calculateBalance();
-    // balanceInnerText.innerText = income - totalExpenses
-    const balanceTotal = parseInt(balanceInnerText.innerText);
-    //Excess Cost Velidation
-    if (balanceTotal < totalExpenses) {
-        balanceInnerText.innerText = "Expense Exceded Your Balance"
-        totalInnerText.innerText = "Reduce Your Expense,Please!"
+    const totalExpenses = parseFloat(totalInnerText.innerText);
+
+    const income = commonInput("income");
+    if (totalExpenses > income) {
+        document.getElementById("balance").innerText = "0"
+        document.getElementById("total-expense").innerText = "Not Enough money"
+    } else {
+        const balanceInnerText = document.getElementById("balance");
+        balanceInnerText.innerText = calculateBalance();
     }
 })
 
-
-
-//saving mony
+///////////////////////////=======Savings Calculation==========\\\\\\\\\\\\\\\\\\\\
 
 document.getElementById("save").addEventListener("click", function () {
     const income = commonInput("income");
-    const savePercentage = document.getElementById("save-percent").value;
-    const savingAmount = (income / 100) * savePercentage;
-    // saving Amount
-    const saveMoney = document.getElementById("save-Amount");
-    saveMoney.innerText = savingAmount;
-    const saveTotal = saveMoney.innerText;
-    //     //remaining Amount
-    const remaining = document.getElementById("remaing-balance");
-    remaining.innerText = calculateBalance() - parseInt(saveTotal);
-    // console.log(remaining.innerText)
+    const balance = calculateBalance();
 
+    const savePercentage = document.getElementById("save-percent").value;
+    const savingAmount = parseFloat(((income / 100) * savePercentage).toFixed(2));
+
+    if (savingAmount > balance || savingAmount < 0) {
+        alert('Your Balance is too low for Savings and put only positive value')
+    } else {
+        const saveMoney = document.getElementById("save-Amount");
+        saveMoney.innerText = savingAmount;
+        const saveTotal = saveMoney.innerText;
+        //remaining Amount
+        // const balance = calculateBalance();
+        const remaining = document.getElementById("remaing-balance");
+        remaining.innerText = parseFloat((balance - parseFloat(saveTotal)).toFixed(2));
+    }
 })
