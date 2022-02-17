f1453c3c40cf8622fb3aa4e6f5761bce5a2dc849
MD-MAHBUBUR-RAHMAN <nirjhor211288@gmapl.com>
2022-02-17 14:31:49 +0600

    Rougf calculation

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 //Common Input Function:
 function commonInput(inputId) {
     const inputItem = document.getElementById(inputId);
-    if (inputItem.value < 0 || inputItem.value == "") {
-        document.getElementById("correction").style.display = "block"
+    if (inputItem.value < 0 || inputItem.value == "" || isNaN(inputItem.value)) {
+        document.getElementById("correction").style.display = "block";
     }
     else {
         const inputValue = inputItem.value;
@@ -13,45 +13,67 @@ function commonInput(inputId) {
     }
 
 }
-
-//Main Calculation Part:
-document.getElementById("calculate").addEventListener("click", function () {
-    //Income input========================
-    const income = commonInput("income");
+function calculationExpense() {
     //Food Input:=======================
     const foodExpense = commonInput("food");
     //Rent Input:==========================
     const rentExpense = commonInput("rent");
     //Clothes Inpute:======================
     const clothExpense = commonInput("cloth");
+
+    const TotalExpence = foodExpense + rentExpense + clothExpense;
+    return TotalExpence;
+}
+function calculateBalance() {
+    //Income input========================
+    const income = commonInput("income");
+    const balance = income - calculationExpense();
+    return balance;
+
+}
+
+//Main Calculation Part:
+document.getElementById("calculate").addEventListener("click", function () {
+    // //Income input========================
+    // const income = commonInput("income");
+    // //Food Input:=======================
+    // const foodExpense = commonInput("food");
+    // //Rent Input:==========================
+    // const rentExpense = commonInput("rent");
+    // //Clothes Inpute:======================
+    // const clothExpense = commonInput("cloth");
     //Total Expenses:========================
     const totalInnerText = document.getElementById("total-expense");
-    totalInnerText.innerText = foodExpense + rentExpense + clothExpense;
+    totalInnerText.innerText = calculationExpense()
+    // totalInnerText.innerText = foodExpense + rentExpense + clothExpense;
     const totalExpenses = parseInt(totalInnerText.innerText);
     //Balance Amount:========================================
     const balanceInnerText = document.getElementById("balance");
-    balanceInnerText.innerText = income - totalExpenses
+    balanceInnerText.innerText = calculateBalance();
+    // balanceInnerText.innerText = income - totalExpenses
     const balanceTotal = parseInt(balanceInnerText.innerText);
     //Excess Cost Velidation
     if (balanceTotal < totalExpenses) {
         balanceInnerText.innerText = "Expense Exceded Your Balance"
         totalInnerText.innerText = "Reduce Your Expense,Please!"
     }
-    //saving amount:
-    document.getElementById("save").addEventListener("click", function () {
-        const income = commonInput("income");
-        const savePercentage = document.getElementById("save-percent").value;
-        const savingAmount = (income / 100) * savePercentage;
-        //saving Amount
-        const saveMoney = document.getElementById("save-Amount");
-        saveMoney.innerText = savingAmount;
-        const saveTotal = saveMoney.innerText;
-        //remaining Amount
-        const remaining = document.getElementById("remaing-balance");
-        remaining.innerText = balanceTotal - parseInt(saveTotal);
+})
 
 
-    })
 
+//saving mony
+
+document.getElementById("save").addEventListener("click", function () {
+    const income = commonInput("income");
+    const savePercentage = document.getElementById("save-percent").value;
+    const savingAmount = (income / 100) * savePercentage;
+    // saving Amount
+    const saveMoney = document.getElementById("save-Amount");
+    saveMoney.innerText = savingAmount;
+    const saveTotal = saveMoney.innerText;
+    //     //remaining Amount
+    const remaining = document.getElementById("remaing-balance");
+    remaining.innerText = calculateBalance() - parseInt(saveTotal);
+    // console.log(remaining.innerText)
 
 })
